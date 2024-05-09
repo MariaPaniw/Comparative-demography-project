2932c1904cc8e24976c7719b44e72babfd9ebb8f
Esin <esin.ickin@uzh.ch>
2024-05-09 11:45:42 +0200

    Update sensitivitiesVR_giraffes.R

--- Files_Giraffes/SensVR/sensitivitiesVR_giraffes.R
+++ Files_Giraffes/SensVR/sensitivitiesVR_giraffes.R
@@ -153,7 +153,7 @@ Results=data.frame(species="Giraffa camelopardalis",
                    stage.age=rep(c("calves","juveniles","adults")),
                    vital.rates="survival",
                    sens=c(Sens_SurvNeo,Sens_SurvJ,Sens_SurvA),
-                   gen.time=5, # Female giraffes attain sexual maturity at approximately 5 years of age and may breed up to age 20 (Dagg and Foster 1976; Dagg 2014
+                   gen.time=6, # from paper: "...females begin giving birth at the age 19 season (= age 6 years)..."
                    n.vr=3, # number of vital rates with covariates
                    n.pam=12, # number of total parameters of these vital rates
                    dens=1,
